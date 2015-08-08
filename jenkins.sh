3541bf0695775a57fae203753d8c078a94e43ada
vagrant <vagrant@homestead>
2015-08-08 08:13:44 +0000

    Jenkins plugins install

--- jenkins.sh
+++ jenkins.sh
@@ -29,6 +29,11 @@ sudo apt-get install jenkins
 # Restart Jenkins
 sudo /etc/init.d/jenkins restart
  
+#Install plugins using Jenkins CLI
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin checkstyle cloverphp crap4j dry htmlpublisher jdepend plot pmd violations warnings xunit
+java -jar jenkins-cli.jar -s http://localhost:8080 safe-restart
+
 # Getting deafault php template
 sudo su jenkins
 cd ~
